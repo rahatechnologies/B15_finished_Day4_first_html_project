dbc3f52540a8ba71bd92d9ea7bea33e1a01c0a97
Piku singh <ps362362@gmail.com>
2023-05-21 11:59:38 +0530

    updated virtual reality

---
+++ fiveserver.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: 8086,
+    //   root: '/',
+    //   open: ['about.html', 'contact.html'],
+    open: ['index.html'],
+    host: '0.0.0.0',
+    browser: 'chrome',
+    https: true,
+  };
