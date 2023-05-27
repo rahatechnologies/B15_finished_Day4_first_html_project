0a0659973448f12b4686c0d4ffd6ff37e27943f0
Piku singh <ps362362@gmail.com>
2023-05-27 18:26:14 +0530

    updated-video-vr

--- fiveserver.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 8086,
-    //   root: '/',
-    //   open: ['about.html', 'contact.html'],
-    open: ['index.html'],
-    host: '0.0.0.0',
-    browser: 'chrome',
-    https: true,
-  };
